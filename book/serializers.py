1468a8d47c9024478136c533e04b606778654025
Yang Xiao <yangxiao@nbp-242-196.nbp.ruw.rutgers.edu>
2018-04-24 11:24:44 -0400

    first commit

---
+++ book/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from book import models as book_models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    User Serializer
+    """
+
+    class Meta:
+        fields = '__all__'
+        model = book_models.User
+
+
+class BookSerializer(serializers.ModelSerializer):
+    """
+    Book Serializer
+    """
+
+    class Meta:
+        fields = '__all__'
+        model = book_models.Book
+
+class UserBookRatingSerializer(serializers.ModelSerializer):
+    """
+    Book Serializer
+    """
+
+    class Meta:
+        fields = '__all__'
+        model = book_models.UserBookRating
